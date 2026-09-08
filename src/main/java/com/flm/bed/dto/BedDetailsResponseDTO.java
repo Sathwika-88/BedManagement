f663f3c602288d093fb61ad6d7d18c55361554a4
sathwikamalka <sathwikamalka@gmail.com>
2026-09-08 23:15:06 +0530

    added BedDetailsResponseDTO

--- src/main/java/com/flm/bed/dto/BedDetailsResponseDTO.java
+++ src/main/java/com/flm/bed/dto/BedDetailsResponseDTO.java
@@ -1,12 +1,14 @@
 package com.flm.bed.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor 
 @NoArgsConstructor
+@Builder 
 public class BedDetailsResponseDTO {
 	
     private long bedNumber;
