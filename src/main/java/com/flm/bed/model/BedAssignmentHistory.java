93afb79ef91d68e7cb5fc7d274f62e478eb50c52
harshareddy-thotli <harshareddy2038@gmail.com>
2026-09-10 14:08:03 +0530

    Add ManyToOne relationship with Bed entity

--- src/main/java/com/flm/bed/model/BedAssignmentHistory.java
+++ src/main/java/com/flm/bed/model/BedAssignmentHistory.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,6 +28,10 @@ public class BedAssignmentHistory {
 	private LocalDateTime assignedAt;
 	
 	private LocalDateTime vacatedAt;
+
+	@ManyToOne
+	@JoinColumn(name = "bed_Number", nullable = false)
+	private Bed bed;
 	
 	
 }
